2b99751ff83dbd6f081203d68f72e8c1b722f80f
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2024-07-06 20:33:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001609607456'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001712303815'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Unavailable4allTime')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
